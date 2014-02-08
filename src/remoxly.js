042289721bd54e4bd0f6df554f30ddecc10a76c3
Ross Cairns <ross@electricglen.com>
2014-02-08 22:13:31 +0000

    Add slider functionality

--- src/remoxly.js
+++ src/remoxly.js
@@ -18,6 +18,9 @@ Panel.prototype.render = function () {
 };
 
 // #Group
+// __Options__
+// * title<string>
+// * isCollapsed<Boolean> _default: true_
 function Group ( options ) {
     var self = this;
 
@@ -74,6 +77,8 @@ Group.prototype.render = function () {
 };
 
 // #Color
+// __Options__
+// * title<string>
 function Color ( options ) {
     this.el = ich.color( {
         title : options.title
@@ -88,20 +93,104 @@ Color.prototype.render = function () {
 };
 
 // #Slider
+// __Options__
+// * title<string>
+// * value<Number> _default: 0_
+// * min<Number> _default: 0_
+// * max<Number> _default: 1_
+// * clamp<Boolean> _default: true_
 function Slider ( options ) {
+    var self = this;
+
     this.el = ich.slider( {
         title : options.title
     });
+    this.max = ( typeof options.max === 'undefined' ) ? 1 : options.max;
+    this.min = ( typeof options.min === 'undefined' ) ? 0 : options.min;
+    this.clamp = ( typeof options.clamp === 'undefined' ) ? true : options.clamp;
+    this.incrementEl = this.el.find( '.tool.inc' );
+    this.decrementEl = this.el.find( '.tool.dec' );
+    this.numberEl = this.el.find( 'h4' );
+    this.sliderEl = this.el.find( 'h3' );
+    this.slideEl = this.el.find( '.slide' );
     this.needsUpdate = true;
+
+    // ## Value
+
+    // ### Property: Value<Boolean>
+    var value = false;
+    Object.defineProperty( this, "value", {
+        get : function () { 
+            return value;
+        },
+        set : function ( v ) { 
+            if ( self.clamp ) {
+                v = clamp( v, self.min, self.max );
+            }
+            if ( v === value ) { return; }
+            value = v;
+            this.needsUpdate = true; 
+        }
+    });
+
+    // ###Events
+    // #### Increment
+    this.incrementEl.on( 'touch click', function ( e ) {
+        self.value += getChangeAmount();
+    });
+    // #### Decrement
+    this.decrementEl.on( 'touch click', function ( e ) {
+        self.value -= getChangeAmount();
+    });
+    // ### Number Input
+    // This won't work on IE
+    this.numberEl.on( 'input', function (e) {
+        var text = self.numberEl.text();
+        if ( !isNaN(text) ) {
+            self.value = +text;
+        }
+    });
+    // When it loses focus update it. This should work in IE
+    // it should also revert the number when it is clamped or NaN
+    this.numberEl.on( 'blur', function (e) {
+        // Delay to ensure value updated
+        setTimeout( function () {
+            var text = self.numberEl.text();
+            if ( !isNaN(text) ) {
+                self.value = +text;
+            }
+            self.needsUpdate = true;
+        }, 0 );
+    });
+
+    // ###Option
+    this.value = ( typeof options.value === 'undefined' ) ? 0 : options.value;
+
+    // Utils
+    function getChangeAmount () {
+        var range = Math.abs( self.min - self.max );
+        if ( range <= 1 ) {
+            return 0.1;
+        } 
+        return 1;
+    }
 }
 
 Slider.prototype.render = function () {
     if ( !this.needsUpdate ) { return; }
-
+    // Set the percntage css of the slide
+    this.slideEl.css( { width : Math.floor( map( this.value, this.min, this.max, 0, 100, true ) ) +'%' } );
+    // Set the number if it does not have focus
+    if ( ! $( document.activeElement ).is( this.numberEl ) ) {
+        this.numberEl.text( floatToString( this.value, 9 ) );
+    }
     this.needsUpdate =  false;
 };
 
 // #Toggle
+// __Options__
+// * title<string>
+// * value<Boolean> _default: false_
 function Toggle ( options ) {
     var self = this;
 
@@ -128,7 +217,6 @@ function Toggle ( options ) {
 
     // ###Event
     this.el.on( 'click touch', function () {
-        console.log(  'touched', value, !value );
         self.value = !value;
     });
 
@@ -147,6 +235,8 @@ Toggle.prototype.render = function () {
 };
 
 // #Button
+// __Options__
+// * title<string>
 function Button ( options ) {
     this.el = ich.button( {
         title : options.title
@@ -160,3 +250,69 @@ Button.prototype.render = function () {
     this.needsUpdate =  false;
 };
 
+// #Utils
+
+// ##Math
+
+function map ( input, inputMin, inputMax, outputMin, outputMax, clamp, ease ) {
+    input = ( input - inputMin ) / ( inputMax - inputMin );
+    if ( ease ) {
+        input = ease(input);
+    } 
+    var output = input * ( outputMax - outputMin ) + outputMin;
+    if ( !!clamp ) {
+        if ( outputMax < outputMin ) {
+            if ( output < outputMax ) {
+                output = outputMax;
+            }
+            else if ( output > outputMin ) {
+                output = outputMin;
+            }
+        } else {
+            if ( output > outputMax ) {
+                output = outputMax;
+            }
+            else if ( output < outputMin ) {
+                output = outputMin;
+            }
+        }
+    }
+    return output;
+}
+
+function clamp ( value, min, max, callbackOnClamp ) {
+    if ( max < min ) {
+        if ( value < max ) {
+            if ( typeof callbackOnClamp === 'function' ) {
+                callbackOnClamp( value, max );
+            }
+            value = max;
+        }
+        else if ( value > min ) {
+            if ( typeof callbackOnClamp === 'function' ) {
+                callbackOnClamp( value, min );
+            }
+            value = min;
+        }
+    } else {
+        if ( value > max ) {
+            if ( typeof callbackOnClamp === 'function' ) {
+                callbackOnClamp( value, max );
+            }
+            value = max;
+        }
+        else if ( value < min ) {
+            if ( typeof callbackOnClamp === 'function' ) {
+                callbackOnClamp(value, min);
+            }
+            value = min;
+        }
+    }
+    return value;
+}
+
+// To remove floating point rounding errors
+function floatToString ( value, precision ) {
+    var power = Math.pow(10, precision || 0);
+    return String(Math.round(value * power) / power);
+}
